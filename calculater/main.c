74769c7a848a0c7be574da679fbd9ccedd52cf6f
Panda Raf <pandaraf3234@gmail.com>
2026-06-02 13:49:07 +0530

    add function

--- calculater/main.c
+++ calculater/main.c
@@ -5,16 +5,57 @@
 #include<stdio.h>
 
 void ad(int a, int b);
+void sub(int a, int b);
+void mlt(int a, int b);
+void div(int a, int b);
 
 int main(){
     int a, b;
+    int choice;
+    char e = 'exit';
     printf("Enter Two  Num : \n");
     scanf("%d %d", &a, &b);
-    ad(a, b);
+    printf("|| Calculater ||\n");
+    printf(" 1. Addition\n 2. subtraction\n 3. multiplication \n 4. dividation\n e. To Exit");
+    scanf("%d", &choice);
+  while(choice == 'exit'){
+    switch (choice)
+    {
+    case 1:
+        ad(a, b);
+        break;
+    case 2:
+        sub(a, b);
+        break;
+    case 3:
+        mlt(a, b);
+        break;
+    case 4:
+        div(a, b);
+    default:
+        printf(">>>invald \n Try Again>>> \n");
+        break;
+    }
+  }
 return 0;
 }
 
 void ad(int a, int b){
      int sum = a + b;
      printf("%d\n", sum);
+}
+
+void sub(int a, int b){
+    int sub = a - b;
+    printf("%d\n", sub);
+}
+
+void mlt(int a, int b){
+    int multi = a * b;
+    printf("%d\n", multi);
+}
+
+void div(int a, int b){
+    int div = a / b;
+    printf("%d\n", div);
 }
