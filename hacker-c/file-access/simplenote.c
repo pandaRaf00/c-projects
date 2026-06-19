ca12399720c094e999d51f460c23f654d7c500d7
Panda Raf <pandaraf3234@gmail.com>
2026-06-19 08:37:42 +0530

    file-accessing and notetaking practice

---
+++ hacker-c/file-access/simplenote.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+
+void usage(char *prog_name,char *filename){
+        printf("Usage: %s <data to add to %s>\n", prog_name, filename);
+}
+
+void fatal(char *);             // a function to fatal error
+void *ec_malloc(unsigned int);  // An error-checked malloc() wrapper
+
+int main(int argc, char *argv[]){
+    int fd;                     // file driscripter
+    char *buffer, *datafile;
+
+    buffer = (char *) ec_malloc(100);
+    datafile = (char *) ec_malloc(20);
+    strcpy(datafile, "/tmp/notes");
+
+    if(argc < 2)                 //If there aren't command line argruments
+        usage(argv[0], datafile);//Display usage messege and exit.
+        strcpy(buffer, argv[1]); // Copy into buffer 
+
+        printf("[DEBUG] buffer @ %p: \'%s\'\n", buffer, buffer);
+        printf("[DEBUG] datafile @ %p: \'%s\'\n", datafile, datafile);
+
+        strncat(buffer, "\n", 1); // Add Newline on the end.
+
+        // *******     Opening FILE      **********
+
+        fd = open(datafile, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+        if(fd == -1);
+        fatal("in main() while opening file");
+        printf("[DEBUG] file descriptor is %d\n", fd);
+
+        // Writing Data
+
+        if(write(fd, buffer, strlen(buffer)) == -1)
+        fatal("in main() while writing buffer to file");
+
+        // Closing File
+        
+        if(close(fd) == -1)
+            fatal("in main() while closing file");
+
+            printf("Note has been saved.\n");
+            free(buffer);
+            free(datafile);
+
+}
+
+// A function to display an error messege and then exit.
+void fatal(char *message){
+    char error_message[100];
+
+    strcpy(error_message, "[!!] Fatal Error");
+    strncat(error_message, message, 83);
+    perror(error_message);
+    exit(-1);
+}
+
+// An error-checked malloc() wrapper function.
+
+void *ec_malloc(unsigned int size){
+    void *ptr;
+    ptr = malloc(size);
+    if(ptr == NULL)
+        fatal("in ec_malloc() on memory allocation.");
+        return ptr;
+}
